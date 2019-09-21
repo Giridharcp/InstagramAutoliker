505ca53f9898f2ae17ed7248445214c8249f2ee1
Giridhar <31183649+Giridharcp@users.noreply.github.com>
2019-09-22 00:28:10 +0530

    Add files via upload

--- instaliker.py
+++ instaliker.py
@@ -1,15 +1,26 @@
+## Make sure you are previously logged in to INSTAGRAM previously itself so to enable the program to function smoothly
+# Refer the documentation of pyautogui for complete understanding of the program.
+
+
 import pyautogui
 import time
 import webbrowser
-i=input("Enter the user name: ")
+name=input("Enter the user name: ")
 webbrowser.open('www.instagram.com')
 time.sleep(5)
+x,y=pyautogui.locateCenterOnScreen(r'/searchbar.PNG')#specify where the searchbar.png is located
+pyautogui.click(x,y)
+time.sleep(0.2)
+pyautogui.type(name)
+time.sleep(0.8)
+pyautogui.press('down')
+pyautogui.press('enter')
 def liker():
               i=0
               try:
                             while(i<=400):
                                           time.sleep(1.2)
-                                          x,y=pyautogui.locateCenterOnScreen(r'C:\Users\Giridhar\Pictures\heart.PNG')
+                                          x,y=pyautogui.locateCenterOnScreen(r'/heart.PNG')#specify where the heart.png is located
                                           pyautogui.click(x-7,y)
                                           time.sleep(0.2)
                                           pyautogui.press('right')
